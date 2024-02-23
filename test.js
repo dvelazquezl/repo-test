04b1364767a3e3714d17562642d49542425b4f02
Daniel Velázquez <daniel545mjj@gmail.com>
2024-02-22 23:37:39 -0300

    remove space

--- test.js
+++ test.js
@@ -1,11 +1,7 @@
-
-
-
-
-
 const string = "texto";
 const char = "c";
 const number = 1;
 const boolean = true;
 const array = [1, 2, 3];
 const object = { key: "value" };
+const undefined = undefined;
