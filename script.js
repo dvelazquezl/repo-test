ef823b21068b059b364bce113767a677f14adcd8
Daniel Velázquez <daniel545mjj@gmail.com>
2024-02-26 10:40:14 -0300

    added tailwindcss, updated ui and added .gitignore

---
+++ script.js
@@ -0,0 +1,52 @@
+console.log("Hello world");
+
+let variable = "hello world";
+function foo() {
+  let variable_2 = "hello world from foo";
+  console.log(variable);
+}
+
+// foo();
+
+const num1 = 230;
+const num2 = 40;
+const num3 = 50;
+
+function test(a, b, c) {
+  let mayor;
+  if (a > b) {
+    mayor = a;
+  } else {
+    mayor = b;
+  }
+
+  if (c > mayor) {
+    mayor = c;
+  }
+  console.log(mayor);
+}
+
+function calc() {
+  let mass = document.getElementById("mass").value;
+  const result = mass * 35;
+  document.getElementById("basal").classList.remove("hidden");
+  document.getElementById("basal").innerHTML = `${result} ml.`;
+}
+
+// test(num1, num2, num3);
+
+function max(data) {
+  let mayor = data[0];
+  for (let i = 1; i < data.length; i++) {
+    if (data[i] > mayor) {
+      mayor = data[i];
+    }
+  }
+  return mayor;
+}
+
+const numbersList = [14, 20, 3, 4, 50, 16, 27, 78, 9, 10];
+
+// console.log(max(numbersList));
+
+// console.log(_.max(numbersList));
--- test.js
+++
@@ -1,7 +0,0 @@
-const string = "texto";
-const char = "c";
-const number = 1;
-const boolean = true;
-const array = [1, 2, 3];
-const object = { key: "value" };
-const undefined = undefined;
